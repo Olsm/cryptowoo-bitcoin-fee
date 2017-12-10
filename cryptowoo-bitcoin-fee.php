c69fc050c4de22f5c9d70d08b5813ffb2caec353
OlavOlsm <OlavOlsm@gmail.com>
2017-12-10 16:42:22 +0100

    add functionality to calculate bitcoin fee and add as surcharge to cart

--- cryptowoo-bitcoin-fee.php
+++ cryptowoo-bitcoin-fee.php
@@ -10,15 +10,56 @@
  * WC tested up to: 3.2.5
  */
 
-add_action( 'woocommerce_cart_calculate_fees','wc_add_surcharge' );
+add_action( 'woocommerce_cart_calculate_fees', 'wc_add_surcharge' );
 
 function wc_add_surcharge() {
-    global $woocommerce;
+	global $woocommerce;
 
-    if ( is_admin() && ! defined( 'DOING_AJAX' ) )
-        return;
+	if ( is_admin() && ! defined( 'DOING_AJAX' ) ) {
+		return;
+	}
 
-    $fee = 1.00;
+	// ToDo: add CryptoWoo setting for fee option
+	/** Options for how fast tx can be forwarded
+	 * hourFee = one hour
+	 * halfHourFee = half hour
+	 * fastestFee = 10 minutes */
+	$fee_option = "hourFee";
 
-    $woocommerce->cart->add_fee( 'Surcharge', $fee, true, 'standard' );
+	try {
+		$fee_satoshi = get_recommended_bitcoin_fee($fee_option);
+		$fee_per_byte = satoshi_to_usd($fee_satoshi);
+		$fee = $fee_per_byte * 226;
+		$woocommerce->cart->add_fee( 'Surcharge (Bitcoin transaction fee)', $fee, true, 'standard' );
+	} catch (Exception $e) {
+		// ToDo: log error message
+		// ToDo: Show error to customer and disable checkout button
+	}
+}
+
+function get_recommended_bitcoin_fee($fee_option) {
+	$request = wp_remote_get( "https://bitcoinfees.earn.com/api/v1/fees/recommended" );
+
+	if ( is_wp_error( $request ) ) {
+		$message = $request->get_error_message();
+		throw new Exception($message);
+	} else if (isset($request['body'])) {
+		$recommended_fees = json_decode( $request['body'] );
+		if ( isset( $recommended_fees->$fee_option ) ) {
+			return $recommended_fees->$fee_option;
+		}
+	}
+
+	throw new Exception("Could not get recommended fee for fee option {$fee_option}");
+}
+
+function satoshi_to_usd($satoshi) {
+	$btc = $satoshi / 100000000;
+	$btc_usd = CW_ExchangeRates::get_exchange_rate("BTC");
+
+	if ( empty($btc_usd) ) {
+		throw new Exception("Could not get usd amount from satoshi amount");
+	}
+
+	return $btc * $btc_usd;
 }
