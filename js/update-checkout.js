904ca1d4abf57c7b4443848b38d89bba84a248a5
OlavOlsm <OlavOlsm@gmail.com>
2017-12-10 22:43:48 +0100

    only update checkout if new payment currency was btc or previous currency was btc

--- js/update-checkout.js
+++ js/update-checkout.js
@@ -1,12 +1,26 @@
 /**
- * Updated the checkout when change the payment method.
+ * Update the checkout when changing the payment currency.
  */
 (function ( $ ) {
+    var previousCurrency = null;
+
 	'use strict';
 
 	$(function () {
 		$( document.body ).on( 'change', 'select[name="payment_currency"]', function () {
-			$( 'body' ).trigger( 'update_checkout' );
+            var element = document.getElementById("payment_currency");
+            if (element.value === "BTC" || previousCurrency === "BTC") {
+                $( 'body' ).trigger( 'update_checkout' );
+            }
+            previousCurrency = element.value;
 		});
+
+        $ ( document.body ).on( 'updated_checkout', function () {
+            var element = document.getElementById("payment_currency");
+            if (previousCurrency) {
+                element.value = previousCurrency;
+            }
+        } );
+
 	});
 }( jQuery ));
