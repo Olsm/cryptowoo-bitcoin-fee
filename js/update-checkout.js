31acf7819bccefacdc7b00d2e9fed4a250bd32ba
OlavOlsm <OlavOlsm@gmail.com>
2017-12-11 15:49:03 +0100

    fix fee not removed if change currency when default currency is btc

--- js/update-checkout.js
+++ js/update-checkout.js
@@ -2,25 +2,33 @@
  * Update the checkout when changing the payment currency.
  */
 (function ( $ ) {
-    var previousCurrency = null;
 
-	'use strict';
+    'use strict';
 
-	$(function () {
-		$( document.body ).on( 'change', 'select[name="payment_currency"]', function () {
+    $(function () {
+        var previous = null;
+
+        $( document.body ).on( 'change', 'select[name="payment_currency"]', function () {
             var element = document.getElementById("payment_currency");
-            if (element.value === "BTC" || previousCurrency === "BTC") {
+            if (element.value === "BTC" || previous === "BTC") {
                 $( 'body' ).trigger( 'update_checkout' );
             }
-            previousCurrency = element.value;
-		});
+            previous = element.value;
+        });
 
-        $ ( document.body ).on( 'updated_checkout', function () {
+        $ ( document.body ).on( 'focus click', 'select[name="payment_currency"]', function () {
             var element = document.getElementById("payment_currency");
-            if (previousCurrency) {
-                element.value = previousCurrency;
+            if (!previous) {
+                previous = element.value;
             }
         } );
 
-	});
+        $ ( document.body ).on( 'updated_checkout', function () {
+            var element = document.getElementById("payment_currency");
+            if (previous) {
+                element.value = previous;
+            }
+        } );
+
+    });
 }( jQuery ));
